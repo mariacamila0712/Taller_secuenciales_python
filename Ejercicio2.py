cfc8011e69d07f9c0d6f2aebe6989a889b201423
MariaCamila <mariacmaza7@gmail.com>
2021-09-03 16:41:54 -0500

    Ejercicio 2 realizado

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -5,3 +5,14 @@ Created on Fri Sep  3 16:23:52 2021
 @author: Maria Camila
 """
 
+# z=5; n=3; m= z-n; y = (((z+2-n)^2 * m+8/2 -30 ) / 2 * 5 -3)^ 5 + 15 * 3 - 9/3
+
+z = 5
+n = 3
+m = z - n
+a = (z + 2 - n) ** 2
+b = m + 8 / 2 - 30
+c = 2 * 5 - 3
+d = 5 + 15 * 3 - 9 / 3
+y = ((a * b) / c) ** d
+print(f'El resultado de y es igual a: {y}')
