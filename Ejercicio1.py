3eca5bf2ab95ca3b029acb9e145ba3f6863762d2
MariaCamila <mariacmaza7@gmail.com>
2021-09-03 17:23:08 -0500

    3 primeros ejercicios verificados

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -7,8 +7,5 @@ This is a temporary script file.
 
 # y = ( (5+2-5)^2 * 5+8/2 -30 ) / 2 * 5 -3
 
-a = (5 + 2 - 5) ** 2
-b = (5 + 8 / 2 - 30)
-c = 2 * 5 - 3
-y = (a * b / c)
-print(f'El resultado de y es igual a: {y}')
+y = ((5 + 2 - 5) ** 2 * 5 + 8 / 2 - 30) / 2 * 5 - 3
+print(f'El valor de y es: {y}')
--- Ejercicio2.py
+++ Ejercicio2.py
@@ -10,9 +10,5 @@ Created on Fri Sep  3 16:23:52 2021
 z = 5
 n = 3
 m = z - n
-a = (z + 2 - n) ** 2
-b = m + 8 / 2 - 30
-c = 2 * 5 - 3
-d = 5 + 15 * 3 - 9 / 3
-y = ((a * b) / c) ** d
+y = (((z+2-n)**2*m+8/2-30)/2*5-3)**5+15*3-9/3
 print(f'El resultado de y es igual a: {y}')
