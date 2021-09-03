eeb7f38a067a96068aded1143033be37259696a3
MariaCamila <mariacmaza7@gmail.com>
2021-09-03 16:24:37 -0500

    Ejercicio 1 desarrollado

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -5,3 +5,10 @@ Spyder Editor
 This is a temporary script file.
 """
 
+# y = ( (5+2-5)^2 * 5+8/2 -30 ) / 2 * 5 -3
+
+a = (5 + 2 - 5)**2
+b = (5 + 8 / 2 - 30)
+c = 2 * 5 - 3
+y = (a * b / c)
+print(f'El resultado de y es igual a: {y}')
---
+++ Ejercicio2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  3 16:23:52 2021
+
+@author: Maria Camila
+"""
+
